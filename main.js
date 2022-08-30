ce0432beda2f02399b32660baf4c6cfff2ef0058
Ariel Navarro <navarro.a.ariel@hotmail.com>
2022-08-29 22:26:47 -0300

    first

---
+++ main.js
@@ -0,0 +1,13 @@
+// new array for shopping cart
+let shoppingCart = []
+
+// global const, wich i'm going to use in this project
+const productsContainer = document.getElementById('products-container');
+const cartContainer =  document.getElementById('cart-container');
+
+const cartCounter = document.getElementById('cartCounter');
+const totalPrice = document.getElementById('totalPrice');
+
+const selectNft = document.getElementById('selectNft');
+const search = document.getElementById('search');
+
