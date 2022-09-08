894c3e616006227fbd740a3ba7669a2fc6488a8c
Ariel Navarro <navarro.a.ariel@hotmail.com>
2022-09-07 22:45:03 -0300

    1er entrega proyecto final

--- stock.js
+++ stock.js
@@ -1,5 +1,13 @@
+
 let stockProductos = [
-    {id:1, nombre:"Bored Ape Yacht Club", precio: 1200, nft: "Bored Ape Yacht Club", img: 'https://lh3.googleusercontent.com/Rn3ZpGAsWIwODp2PdC21GgROWTYlxvK7xys1amJ7Z-HIEg3l4cN0ReAg_u572Xfp05GskW913KAu5tQyYThlbJVrc5ZS297Dfykosw=w600', cantidad: 1},
+    {   
+        id:1, 
+        nombre:"Bored Ape Yacht Club", 
+        precio: 1200, 
+        nft: "Bored Ape Yacht Club", 
+        img: 'https://lh3.googleusercontent.com/Rn3ZpGAsWIwODp2PdC21GgROWTYlxvK7xys1amJ7Z-HIEg3l4cN0ReAg_u572Xfp05GskW913KAu5tQyYThlbJVrc5ZS297Dfykosw=w600', 
+        cantidad: 1
+    },
     {id:2, nombre:"Bored Ape Yacht Club", precio: 1100, nft: "Bored Ape Yacht Club", img: 'https://lh3.googleusercontent.com/8pSQDqVgCuRx0eZ666DwHlQPqppiRK95_45V-L_dhZbyREo0IIX0K6vNCsnqahKBrzG7Vjt6CfsojZx86PAorcthLeki4vNuBxmTQg=w600', cantidad: 1},
     {id:3, nombre:"Bored Ape Yacht Club", precio: 1200, nft: "Bored Ape Yacht Club", img: 'https://lh3.googleusercontent.com/oGX9KC5HSLaNBtg3pNyJvlMca_YwhR3xvSllayk86pghLCOJkvbQ8DzNuiF2ZY-HU_ZzYayTZngFnp9kJdi8J140dtErziZcfu2jUw=w600', cantidad: 1},
     {id:4, nombre:"Bored Ape Yacht Club", precio: 1400, nft: "Bored Ape Yacht Club", img: 'https://lh3.googleusercontent.com/-PwKyfcTNhTCiTWEff1prU-qPHunTYZfCsuJsZ_5Z_AFru6PT_cxPSYvUAuenHYQ46PU2Xq1umJqb0n1qgqXYO9Smcw_zcN2-jKL=w600', cantidad: 1},
